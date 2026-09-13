2ce10862da56881f9ad832c8a230029c584ba737
tkhy2651 <tah331925@gmail.com>
2026-09-13 14:34:37 +0900

    feat: onloadメソッドにリボンアイコンを追加し、リロード機能を実装

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { Plugin } from "obsidian";
 
 export default class QuickReloadPlugin extends Plugin {
     onload() {
-
+        this.addRibbonIcon("refresh-cw", "Quick Reload", () => {
+            window.location.reload();
+        });
     }
 }
